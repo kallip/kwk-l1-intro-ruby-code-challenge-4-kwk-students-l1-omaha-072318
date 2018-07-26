446454c497ee7c946e93807910a80c8923120e49
kallip <kallinpettit@gmail.com>
2018-07-26 21:16:08 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -3,28 +3,29 @@
 def least_coins(coins)
 
 #Code your answer here!
-while
-coins = {"quarters" => 0, "dimes" => 0, "nickles" => 0, "pennies" => 0}
+# while
+# coins = {"quarters" => 0, "dimes" => 0, "nickles" => 0, "pennies" => 0}
+# end
+
+# while least_coins => 25
+# coin["quarters"] += 1 
+# cents -= 25
+# end
+
+# while least_coins => 10
+# coin["dimes"] += 1
+# cents -= 10
+# end
+
+# while least_coins => 5
+# coin["nickles"] += 1
+# cents -= 5
+# end
+
+# while least_coins => 1
+# coin["pennies"] += 1
+# cents -= 1
+# puts least_coins
+# end
 end
 
-while least_coins => 25
-coin["quarters"] += 1 
-cents -= 25
-end
-
-while least_coins => 10
-coin["dimes"] += 1
-cents -= 10
-end
-
-while least_coins => 5
-coin["nickles"] += 1
-cents -= 5
-end
-
-while least_coins => 1
-coin["pennies"] += 1
-cents -= 1
-end
-end
-puts least_coins
